6a8c3ce1b0ce3b5e8b9db9f588df76f5a229b4fa
Sam Brannen <sam@sambrannen.com>
2017-10-03 18:35:03 -0700

    Update based on live coding

--- src/test/java/working/TagsDisplayNamesAndDependencyInjectionTests.java
+++ src/test/java/working/TagsDisplayNamesAndDependencyInjectionTests.java
@@ -2,6 +2,7 @@
 package working;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
 
@@ -11,17 +12,18 @@ class TagsDisplayNamesAndDependencyInjectionTests {
 
 	// --include-tag fast
 	// --exclude-tag fast
+
 	// @Test
 	// @Tag("fast")
 	@FastTest
-	@DisplayName("Fast test with a cool name! 😱")
+	@DisplayName("Fast test! 😱")
 	void fastTest(TestInfo testInfo) {
-		System.out.println(testInfo);
+		System.err.println(testInfo);
 	}
 
+	@Tag("normal")
 	@Test
-	void normalTest(TestInfo testInfo) {
-		System.out.println(testInfo);
+	void normalTest() {
 	}
 
 }
