c3a2977d6986d227740b3c06170a0bd802d8ce65
Sam Brannen <sam@sambrannen.com>
2017-12-06 17:20:25 -0800

    Add scaffolding for SpringBootJupiterTests

--- src/test/java/spring/SpringBootJupiterTests.java
+++ src/test/java/spring/SpringBootJupiterTests.java
@@ -1,6 +1,23 @@
 
 package spring;
 
+import demo.Cat;
+
 class SpringBootJupiterTests {
+
+	final Cat primaryCat;
+
+	SpringBootJupiterTests(Cat primaryCat) {
+		this.primaryCat = primaryCat;
+	}
+
+	void primaryCat() {
+	}
+
+	void qualifiedCat() {
+	}
+
+	void cats() {
+	}
 
 }
